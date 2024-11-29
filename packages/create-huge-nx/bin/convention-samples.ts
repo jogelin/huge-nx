d7d94c44c6cbfc3717a8dbe891f3cc01c9fe1465
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-11-29 17:36:39 +0100

    chore(samples): Move samples in separate folder

---
+++ packages/create-huge-nx/bin/convention-samples.ts
@@ -0,0 +1,7 @@
+export const conventionSamples = [
+  'huge-angular-full-stack',
+  'huge-angular-mf',
+  'nx-preset-angular-monorepo',
+  'nx-preset-angular-monorepo-v18',
+  'nx-preset-react-monorepo',
+];
