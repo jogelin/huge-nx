cd7fff1775b2dc6dec7072879e447fbd18cddd9f
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-12-12 17:15:53 +0100

    fix(format): fix formatting

--- packages/plugin/src/utils/is-matching-plugin.utils.ts
+++ packages/plugin/src/utils/is-matching-plugin.utils.ts
@@ -2,10 +2,7 @@ import { CreateNodesV2 } from '@nx/devkit';
 import { CreateNodesInternal } from './create-nodes-internal-builder.utils';
 import { minimatch } from 'minimatch';
 
-export function isMatchingPlugin<T extends Record<string, unknown>>(
-  createNodes: CreateNodesV2<T> | CreateNodesInternal<T>,
-  filePath: string
-) {
+export function isMatchingPlugin<T extends Record<string, unknown>>(createNodes: CreateNodesV2<T> | CreateNodesInternal<T>, filePath: string) {
   const pluginPattern = createNodes[0];
   return minimatch(filePath, pluginPattern);
 }
