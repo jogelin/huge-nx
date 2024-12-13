6e087efa905378c3b4a0028da46509fe1d3ebf25
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-12-13 15:16:05 +0100

    ci(test): test ci

--- e2e/examples/tests/create-huge-nx/with-conventions-file-path.spec.ts
+++ e2e/examples/tests/create-huge-nx/with-conventions-file-path.spec.ts
@@ -21,9 +21,6 @@ describe(`e2e: ${conventionsFile}`, () => {
         recursive: true,
         force: true,
       });
-    } else {
-      console.log(`Test failed - Workspace content at ${wsCwd}:`);
-      runCommand('ls -la', wsCwd);
     }
   });
 
