cd1d5e0046dd7776ec5503a7a857966e3040b9d2
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-12-16 16:37:58 +0100

    fix(ci): fix nx-cloud not found

--- e2e/utils/global-setup.ts
+++ e2e/utils/global-setup.ts
@@ -1,9 +1,12 @@
+import { execSync } from 'node:child_process';
 import { startLocalRegistryAndRelease, stopLocalRegistry } from './local-registry.utils';
 
 let teardownHappened = false;
 
 export async function setup() {
   await startLocalRegistryAndRelease();
+
+  execSync('npm install -g create-huge-nx', { stdio: 'inherit' });
 }
 
 export async function teardown() {
