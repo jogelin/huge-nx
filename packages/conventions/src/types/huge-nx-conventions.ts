aaa00a80d8d47c443b7f5305a0124b2001ee3c23
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-12-19 12:03:57 +0100

    doc(conventions): Add ts doc on conventions

--- packages/conventions/src/types/huge-nx-conventions.ts
+++ packages/conventions/src/types/huge-nx-conventions.ts
@@ -31,9 +31,24 @@ export type HugeNxWorkspace =
     };
 
 export interface HugeNxConventions {
+  /**
+   * The version of the conventions file.
+   */
   version: `${number}.${number}`;
+  /**
+   * This is nothing new and is **already available** in Nx by configuring your [`nx.json`](https://nx.dev/reference/nx-json#generators) file. You can define default options for each generator that you are using in your workspace.
+   * All Nx options can be found in the [Nx API Documentation](https://nx.dev/nx-api).
+   */
   generators: OptionsByGenerator;
+  /**
+   * Here you'll define your list of **ProjectType** based on the technologies, the domain, the type of library, the team, etc.
+   * For each **ProjectType**, you'll specify which generators should be used and all conventions around them. It will use the **Default Generator Options**, and you can add extra options if needed.
+   */
   projectTypes: HugeNxProjectTypes;
+  /**
+   * Finally, you'll define a seed that will look like your desired workspace. Each project will be linked and described by a specific **ProjectType**.
+   * This seed can be used to generate a new workspace or a workspace that look that yours with the latest Nx version for example.
+   */
   workspace: HugeNxWorkspace;
 }
 
