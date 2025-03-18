f758cd8d41a81110084017fe02c1858ce24f546b
James Gibson <james.the.nodester@gmail.com>
2025-03-18 03:06:21 -0600

    Update inverted logic boolean check

--- packages/devkit/src/lib/verbose.ts
+++ packages/devkit/src/lib/verbose.ts
@@ -11,4 +11,4 @@ export const isVerbose = () => IS_VERBOSE_REQUESTED || process.argv.includes('--
 
 export const STDIO_OUTPUT: 'ignore' | 'inherit' | 'pipe' = (IS_QUIET_REQUESTED && !isVerbose()) ? 'ignore' : 'inherit';
 
-export const output = (isVerbose() && IS_QUIET_REQUESTED ? _output : silentOutput) as unknown as CLIOutput;
+export const output = (isVerbose() && !IS_QUIET_REQUESTED ? _output : silentOutput) as unknown as CLIOutput;
